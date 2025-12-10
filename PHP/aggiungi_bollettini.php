19f40d254f7ed619f3026cc0ace0834c8f14667b
momoo07 <mohammed.elgouraini@buonarroti.tn.it>
2025-12-10 23:08:55 +0100

    modifica bollettino

--- PHP/aggiungi_bollettini.php
+++ PHP/aggiungi_bollettini.php
@@ -1,21 +1,46 @@
 <?php
-$percorso = "../JSON/bollettini.json";
+
+
+$percorsoJSON = "../JSON/bollettini.json";
+
+$cartellaPDF = "../PDF/";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+
     $titolo = $_POST["titolo"];
     $data = $_POST["data"];
-    $file = $_POST["file"];
 
-    $json = file_get_contents($percorso);
+    // Prendo il nome del file caricato
+    $nomeFile = $_FILES["file"]["name"];
+
+    // Prendo il percorso temporaneo del file (cartella temporanea del server)
+    $tmpFile = $_FILES["file"]["tmp_name"];
+
+    // Percorso finale dove salvare il file
+    $percorsoFile = $cartellaPDF . $nomeFile;
+
+    
+
+    // Sposto il file dalla cartella temporanea alla cartella PDF
+    move_uploaded_file($tmpFile, $percorsoFile);
+
+    // Leggo il contenuto del JSON
+    $json = file_get_contents($percorsoJSON);
+
+  
     $dati = json_decode($json, true);
 
+  
     $dati["bollettini"][] = [
         "titolo" => $titolo,
-        "data" => $data,
-        "file" => $file
+        "data"   => $data,
+        "file"   => "../PDF/" . $nomeFile
     ];
 
-    file_put_contents($percorso, json_encode($dati, JSON_PRETTY_PRINT));
+    // Salvo il JSON aggiornato
+    file_put_contents($percorsoJSON, json_encode($dati, JSON_PRETTY_PRINT));
+
 
     header("Location: ../Pagine/modifiche.html");
     exit;
